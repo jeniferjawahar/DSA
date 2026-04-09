15a3f44cd09cd14727d5b571a7c3029d752c2bd9
jenifer-xavi <jeniferx@stgit.com>
2026-04-09 18:06:11 +0530

    moveSeroes

--- logiclearn/test.js
+++ logiclearn/test.js
@@ -493,3 +493,22 @@
 // }
 
 
+//move all zeroes to the end of an array
+
+function moveZeroes(arr){
+    let j=0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]!==0){
+            arr[j]=arr[i];
+            j++;
+        }
+    }
+    while(j<arr.length){
+        arr[j]=0;
+        j++;
+    }
+
+    return arr;
+}
+
+console.log(moveZeroes([0,1,0,3,12]))
